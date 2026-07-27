12dac6912a4bf0f222a9717c6dbd72fe25279eea
Eric Haiara <eric.haiara10@gmail.com>
2026-07-28 09:29:36 +1000

    Update db.py

--- backend/core/db.py
+++ backend/core/db.py
@@ -4,7 +4,6 @@ import uuid
 from datetime import datetime, timezone
 
 from motor.motor_asyncio import AsyncIOMotorClient
-
 MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
 DB_NAME = os.getenv("DB_NAME", "trel_db")
 
